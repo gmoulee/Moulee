6c384fe5972db3acd97a6c05a222760bdeaaff1d
Mouleeswaran Ganapathi <moulee@outlook.com>
2019-10-21 20:50:41 +0200

    Changes with comment

--- Moulee/src/Ceiling.java
+++ Moulee/src/Ceiling.java
@@ -5,9 +5,15 @@ public class Ceiling{
 	
 	public static int findCeiling(int[] arr,int start, int end, int k) {
 		int index = Integer.MAX_VALUE;
-		for(int i=start;i<=end;i++) {
-			if(arr[i] >= k) {
-				index = arr[i];
+		if(arr[start] >= k) {
+			index = arr[start];
+		}
+		for(int i=start+1;i<=end;i++) {
+			if(arr[i] == k) {
+				return arr[i];				// If the element is present in the array return the element.
+			}
+			if(arr[i] > k && arr[i-1] < k) {
+				index = arr[i];				// The smallest element greater then k is the ceiling.
 			}
 		}
 		return index;
@@ -16,7 +22,7 @@ public class Ceiling{
 	public static void main(String[] args) {
 		int[ ] arr = {1,5,10,15,25};
 		int len = arr.length;
-		int k = 0;
+		int k = 1;
 		int index = findCeiling(arr, 0, len-1, k);
 		if(index == Integer.MAX_VALUE) {
 			System.out.print("ceiling not found");
